d1674884567168056051e3d010360ba9cfa31f7b
JerryWang0122 <school832@gmail.com>
2024-05-27 14:26:27 +0800

    feat: jwt with expiration time

---
+++ src/main/java/security/jwt/SimpleLimitedJWT.java
@@ -0,0 +1,50 @@
+package security.jwt;
+
+import com.nimbusds.jwt.JWTClaimsSet;
+import net.glxn.qrgen.QRCode;
+import security.KeyUtil;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Date;
+
+// 建立一個高鐵票的 JWT，有時效性(當日有效票)
+public class SimpleLimitedJWT {
+    public static void main(String[] args) throws Exception {
+        // 1. 高鐵公司的簽名專用密鑰 (JWK)
+        String signingSecure = "abcdefghijklmnopqrstuvwxyz123456";  // 256bits -> 32bytes
+
+        Date time = new Date(new Date().getTime() + (5 * 1000));    // 現在時刻加十秒
+        // 2. 建立 payload (創建 JWT 的聲明 claims) 裡面就是票務資訊
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+                .subject("高鐵票")
+                .issuer("台灣高鐵")
+                .claim("authcode", "082E")
+                .expirationTime(time) // 設定有效期限
+                .build();
+
+        // 3. 對 JWT 進行簽名，並取得 token
+        String token = KeyUtil.signJWT(claimsSet, signingSecure);
+
+        System.out.printf("高鐵票 token: %s%n", token);
+
+        // 4. 產生QRCode
+        File file = new File("src/main/java/security/jwt/ticket_qrcode.png");
+        QRCode.from(token).withSize(300, 300).writeTo(new FileOutputStream(file));
+        System.out.println("QRCode 產生完畢");
+
+        // ------------------------------------------------------------------------
+        while (true) {
+
+            // 5. 驗證 JWT 簽名
+            if (KeyUtil.verifyJWTSignature(token, signingSecure)) {
+                System.out.println("JWT 簽名驗證成功");
+                System.out.println("驗票閘門開啟...");
+            } else {
+                System.out.println("JWT 簽名驗證失敗/token過期");
+                break;
+            }
+            Thread.sleep(2000);
+        }
+    }
+}
